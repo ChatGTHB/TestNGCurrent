07fddc607da388494ea0f8f2206c6be1d25a82fd
ChatGTHB <cihat.kose@hotmail.com>
2023-09-02 16:52:12 +0300

    Added BaseDriver

---
+++ src/utility/BaseDriver.java
@@ -0,0 +1,55 @@
+package utility;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+    public static WebDriver driver; // SingletonDriver method
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void initialOperations() {  // The condition of this is that it is extends and takes place in the first place.
+        Logger logger = Logger.getLogger(""); // Get output logs.
+        logger.setLevel(Level.SEVERE);              // Show only ERRORs
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize(); // It maximizes the screen.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 30 sec delay: time to load the page
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));  // 30 sec delay: time to find the element
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        loginTest();
+    }
+
+    public void loginTest() {
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+        MyFunction.wait(2);
+
+        WebElement inputEmail = driver.findElement(By.id("input-email"));
+        inputEmail.sendKeys("testng1@gmail.com");
+
+        WebElement inputpassword = driver.findElement(By.id("input-password"));
+        inputpassword.sendKeys("123qweasd");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        loginBtn.click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Account"));
+    }
+
+    @AfterClass
+    public static void finishingOperations() {
+        MyFunction.wait(5);
+        driver.quit();
+    }
+}
