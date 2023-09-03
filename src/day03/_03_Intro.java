b58a857b3047203883214a0b56a1a1de26d9c9cb
ChatGTHB <cihat.kose@hotmail.com>
2023-09-03 10:40:43 +0300

    Added _03_Intro

---
+++ src/day03/_03_Intro.java
@@ -0,0 +1,18 @@
+package day03;
+
+import org.testng.annotations.Test;
+
+public class _03_Intro {
+
+    @Test
+    public void Intro3_Test1() {
+        System.out.println("Intro3 -> Test1");
+    }
+
+    @Test
+    public void Intro3_Test2() {
+        System.out.println("Intro3 -> Test2");
+    }
+
+
+}
