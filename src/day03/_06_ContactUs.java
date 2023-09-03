6fcc5f0f1dd0fcbe1acb351501ec1036ca84d15f
ChatGTHB <cihat.kose@hotmail.com>
2023-09-03 10:41:19 +0300

    Added _06_ContactUs

---
+++ src/day03/_06_ContactUs.java
@@ -0,0 +1,40 @@
+package day03;
+
+
+import utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _06_ContactUs extends BaseDriver {
+
+    /**
+     Scenario:
+     1- Click ContactUs
+     2- Write a message of at least 10 characters in the message box.
+     3- After submitting, verify the success text in the URL.
+
+     ---------------------------------------------------------------
+
+     Senaryo:
+     1- ContactUs'a tıklayınız
+     2- Mesaj kutusuna en az 10 karakterlik bir mesaj yazınız.
+     3- Submit ettikten sonra URL'deki success yazısını doğrulayınız.
+     */
+
+    @Test
+    public void contactUs(){
+
+        WebElement contactUsBtn= driver.findElement(By.linkText("Contact Us"));
+        contactUsBtn.click();
+
+        WebElement enquiryArea= driver.findElement(By.id("input-enquiry"));
+        enquiryArea.sendKeys("Hello testing world!");
+
+        WebElement submitButton= driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("success"));
+    }
+}
