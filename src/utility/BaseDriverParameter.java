0662e83df453ac23291c21c20851c96b486c5751
ChatGTHB <cihat.kose@hotmail.com>
2023-09-03 10:34:50 +0300

    Added BaseDriverParameter

---
+++ src/utility/BaseDriverParameter.java
@@ -0,0 +1,78 @@
+package utility;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriverParameter {
+
+    public WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    @Parameters("browserType")
+    public void initialOperations(String browserType) {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        switch (browserType.toLowerCase()) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+
+            case "safari":
+                driver = new SafariDriver();
+                break;
+
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+
+            default:
+                driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize(); // It maximizes the screen.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 30 sec delay: time to load the page
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));  // 30 sec delay: time to find the element
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        loginTest();
+    }
+
+    public void loginTest() {
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+        MyFunction.wait(2);
+
+        WebElement inputEmail = driver.findElement(By.id("input-email"));
+        inputEmail.sendKeys("testng1@gmail.com");
+
+        WebElement inputpassword = driver.findElement(By.id("input-password"));
+        inputpassword.sendKeys("123qweasd");
+
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@type='submit']"));
+        loginBtn.click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Account"));
+    }
+
+    @AfterClass
+    public void finishingOperations() {
+        MyFunction.wait(5);
+        driver.quit();
+    }
+}
