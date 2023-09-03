622b8611c5463e2ddc543dd95947b750a9a01a5d
ChatGTHB <cihat.kose@hotmail.com>
2023-09-03 10:41:30 +0300

    Added _07_ContactUsParameter

---
+++ src/day03/_07_ContactUsParameter.java
@@ -0,0 +1,41 @@
+package day03;
+
+
+import utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class _07_ContactUsParameter extends BaseDriver {
+
+    /**
+     * Scenario:
+     1- Click ContactUs
+     2- Write a message of at least 10 characters in the message box.
+     3- After submitting, verify the success text in the url.
+     ---------------------------------------------------------------
+
+     Senaryo:
+     1- ContactUs'a tıklayınız
+     2- Mesaj kutusuna en az 10 karakterlik bir mesaj yazınız.
+     3- Submit ettikten sonra url'deki success yazısını doğrulayınız.
+     */
+
+    @Test
+    @Parameters("message") // must be the same name as in xml
+    public void contactUs(String incomingMessage){
+
+        WebElement contactUsBtn= driver.findElement(By.linkText("Contact Us"));
+        contactUsBtn.click();
+
+        WebElement enquiryArea= driver.findElement(By.id("input-enquiry"));
+        enquiryArea.sendKeys(incomingMessage);
+
+        WebElement submitButton= driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("success"));
+    }
+}
