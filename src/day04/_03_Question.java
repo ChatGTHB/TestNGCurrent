7df8c1ac05e26dababcd9d501b0af005388ef323
ChatGTHB <cihat.kose@hotmail.com>
2023-09-04 12:34:44 +0300

    Added _03_Question

---
+++ src/day04/_03_Question.java
@@ -0,0 +1,27 @@
+package day04;
+
+
+
+public class _03_Question {
+
+/**
+ * Scenario:
+ * 1- Go to the site,
+ * 2- Click on Specials,
+ * 3- Inquire about the products (is there a discount on all products, is the old price higher than the new price)
+ * 4- After the above process, the previous price of each product will be changed from the current price.
+ * Verify that it is high.
+ * 5- Test the above process in 2 different browsers and in parallel.
+ * <p>
+ * -----------------------------------------------------------------------------------------------------------------
+ * <p>
+ * Senaryo:
+ * 1- Siteye gidiniz,
+ * 2- Specials'e tıklayınız,
+ * 3- Çıkan ürünlerde (bütün ürünlerde indirim var mı eski fiyat yeni fiyattan büyük mü) şeklinde sogulama yapınız
+ * 4- Yukarıdaki işlemden sonra her ürünün önceki fiyatının şu andaki fiyattan
+ * yüksek olduğunu doğrulayınız.
+ * 5- Yukarıdaki işlemi 2 farklı browserda ve paralel şekilde test ediniz.
+ */
+
+}
