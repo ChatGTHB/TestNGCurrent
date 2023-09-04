af34a42b3d96c4538e568373714799f26c01e44e
ChatGTHB <cihat.kose@hotmail.com>
2023-09-04 12:33:49 +0300

    Added _01_SearchFunctionality

---
+++ src/day04/_01_SearchFunctionality.java
@@ -0,0 +1,46 @@
+package day04;
+
+
+import utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _01_SearchFunctionality extends BaseDriver {
+    /**
+     * Scenario:
+     * 1- Open the site.
+     * 2- Search by sending the word mac.
+     * 3- Verify that the word "mac" is included in the results.
+     * 4- Do the same for Samsung.
+     * <p>
+     * ------------------------------------------------------------
+     * <p>
+     * Senaryo:
+     * 1- Siteyi açınız.
+     * 2- mac kelimesini göndererek aratınız.
+     * 3- Çıkan sonuçlarda "mac" kelimesinin geçtiğini doğrulayınız.
+     * 4- Aynı işlemi samsung için de yapınız.
+     */
+
+    @Test
+    @Parameters("searchText")
+    void SearchFunction(String searchedWord) {
+
+        WebElement searchInput = driver.findElement(By.name("search"));
+        searchInput.sendKeys(searchedWord + Keys.ENTER);
+
+        List<WebElement> captions = driver.findElements
+                (By.cssSelector("[class='caption'] > h4"));
+
+        for (WebElement caption : captions) {
+            //System.out.println("caption.getText() = " + caption.getText());
+            Assert.assertTrue(caption.getText().toLowerCase().contains(searchedWord), "search word not found");
+        }
+    }
+}
