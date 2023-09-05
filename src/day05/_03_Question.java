161fc9088bfe216ebf1eb2a178e9ec6ec86b49f9
ChatGTHB <cihat.kose@hotmail.com>
2023-09-05 07:56:10 +0300

    Added _03_Question

---
+++ src/day05/_03_Question.java
@@ -0,0 +1,45 @@
+package day05;
+
+
+import utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _03_Question extends BaseDriver {
+
+    /**
+     * Do the Search function that we did in the previous lessons with dataprovider for mac, ipod and samsung.
+     * <p>
+     * ----------------------*****************************************---------------------------------------
+     * <p>
+     * Daha önceki derslerde yaptğımız Search fonksiyonunu mac,ipod ve samsung için dataprovider ile yapınız.
+     */
+
+    @Test(dataProvider = "getData")
+    public void SearchFunction(String willBeSearchText) {
+
+        WebElement searchInput = driver.findElement(By.name("search"));
+        searchInput.clear();
+        searchInput.sendKeys(willBeSearchText + Keys.ENTER);
+
+        List<WebElement> captions = driver.findElements(By.cssSelector("[class='caption'] > h4"));
+
+        for (WebElement caption : captions) {
+            //System.out.println("caption.getText() = " + caption.getText());
+            Assert.assertTrue(caption.getText().toLowerCase().contains(willBeSearchText), "Search word not found.");
+        }
+    }
+
+    @DataProvider
+    Object[] getData() {
+        Object[] data = {"mac", "samsung", "ipod"};
+
+        return data;
+    }
+}
