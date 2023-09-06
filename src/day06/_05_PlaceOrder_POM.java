94eeaf4c074fe60e1c09177695099b08a696a160
ChatGTHB <cihat.kose@hotmail.com>
2023-09-06 12:29:40 +0300

    Added _05_PlaceOrder_POM

---
+++ src/day06/_05_PlaceOrder_POM.java
@@ -0,0 +1,56 @@
+package day06;
+
+import utility.BaseDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _05_PlaceOrder_POM extends BaseDriver {
+
+    /**
+     * Scenario :
+     * 1- Open the site.
+     * 2- Search the word "ipod" on the site
+     * 3- Put the first of the results in the basket.
+     * 4- Click on Shopping Chart.
+     * 5- Checkout.
+     * 6- Click Continue buttons and enter the information.
+     * 7- Confirm that you have placed the order with en confirm.
+     * Note: Verify with the text "Your order has been placed" on the page that appears.
+     * <p>
+     * Senaryo :
+     * 1- Siteyi açınız.
+     * 2- Sitede "ipod" kelimesini aratınız.
+     * 3- Çıkan sonuçlardan ilkini sepete atınız.
+     * 4- Shopping Carta tıklatınız.
+     * 5- Checkout yapınız.
+     * 6- Continue butonlarına tıklatıp bilgileri giriniz.
+     * 7- En confirm ile siparişi verdiğinizi doğrulayınız.
+     * Not: Doğrulamayı çıkan sayfadaki "Your order has been placed" yazısı ile yapınız.
+     */
+
+
+    @Test
+    public void ProceedToCheckout() {
+
+        _04_PlaceOrder_Elements elements = new _04_PlaceOrder_Elements();
+
+        elements.searchBox.sendKeys("ipod" + Keys.ENTER);
+        elements.addToChart.click();
+        elements.shoppingChart.click();
+        elements.checkOut.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(elements.continue1)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements.continue2)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements.continue3)).click();
+        elements.agree.click();
+        wait.until(ExpectedConditions.elementToBeClickable(elements.continue4)).click();
+
+        elements.confirm.click();
+
+        wait.until(ExpectedConditions.urlContains("success"));
+
+        Assert.assertTrue(elements.msg.getText().equals("Your order has been placed!"));
+    }
+}
