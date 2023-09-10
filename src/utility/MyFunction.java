36bf688a1ffe684168d5b22754b017fd0803c783
ChatGTHB <cihat.kose@hotmail.com>
2023-09-10 23:20:54 +0300

    Updated MyFunction

--- src/utility/MyFunction.java
+++ src/utility/MyFunction.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+import java.util.List;
+
 public class MyFunction {
     public static void wait(int sec) {
 
@@ -15,7 +17,22 @@ public class MyFunction {
     }
 
     public static void successMessageValidation() {
-        WebElement msgLabel = BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
-        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+        WebElement messageLabel = BaseDriver.driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']"));
+        Assert.assertTrue(messageLabel.getText().toLowerCase().contains("success"));
+    }
+
+    public static int randomGenerator(int limit) {
+        return (int) (Math.random() * limit);
+    }
+
+    public static boolean listContainsString(List<WebElement> list, String aranacakKelime) {
+        boolean found = false;
+        for (WebElement e : list) {
+            if (e.getText().toLowerCase().equals(aranacakKelime.toLowerCase())) {
+                found = true;
+                break;
+            }
+        }
+        return found;
     }
 }
