b570febf20945204e00f19411e52c6cac763f3dc
ChatGTHB <cihat.kose@hotmail.com>
2023-09-19 13:13:06 +0200

    Updated _01_WishList

--- src/day07/_01_WishList.java
+++ src/day07/_01_WishList.java
@@ -14,21 +14,21 @@ import java.util.List;
 public class _01_WishList extends BaseDriver {
 
     /**
-     Scenario ;
-     1- Go to the site...
-     2- Search "ipod" product, send "ipod" from xml
-     3- Click on the Add Wish button of a random element among the resulting elements.
-     4- Then click on WishList
-     5- Verify whether the name of the product that appears here is the same as the product that was clicked.
-
-     ========================================================================================================
-
-     Senaryo ;
-     1- Siteye gidiniz.
-     2- "ipod" ürününü aratınız."ipod" u xml den gönderiniz
-     3- Çıkan elemanlardan random bir elemanın Add Wish butonuna tıklayınız.
-     4- Daha sonra WishList e tıklatınız
-     5- Burada çıkan ürünle tıklanan ürünün isminin aynı olup olmadığını doğrulayınız.
+     * Scenario ;
+     * 1- Go to the site...
+     * 2- Search "ipod" product, send "ipod" from xml
+     * 3- Click on the Add Wish button of a random element among the resulting elements.
+     * 4- Then click on WishList
+     * 5- Verify whether the name of the product that appears here is the same as the product that was clicked.
+     * <p>
+     * ========================================================================================================
+     * <p>
+     * Senaryo ;
+     * 1- Siteye gidiniz.
+     * 2- "ipod" ürününü aratınız."ipod" u xml den gönderiniz
+     * 3- Çıkan elemanlardan random bir elemanın Add Wish butonuna tıklayınız.
+     * 4- Daha sonra WishList e tıklatınız
+     * 5- Burada çıkan ürünle tıklanan ürünün isminin aynı olup olmadığını doğrulayınız.
      */
 
     @Test
@@ -38,27 +38,27 @@ public class _01_WishList extends BaseDriver {
         WebElement searchBox = driver.findElement(By.name("search"));
         searchBox.sendKeys(searchWord + Keys.ENTER);
 
-        List<WebElement> wishButtons=driver.findElements(  // Wish buttons of products
+        List<WebElement> wishButtons = driver.findElements(  // Wish buttons of products
                 By.xpath("//button[@data-original-title='Add to Wish List']"));
 
-        List<WebElement> productList=driver.findElements( // Titles of products
+        List<WebElement> productList = driver.findElements( // Titles of products
                 By.xpath("//div[@class='caption']//h4"));
 
-        int randomSelection= MyFunction.randomGenerator(productList.size()); // A random number is generated
-        String wishItemText=productList.get(randomSelection).getText();      // The name of the product in random was taken
+        int randomSelection = MyFunction.randomGenerator(productList.size()); // A random number is generated
+        String wishItemText = productList.get(randomSelection).getText();      // The name of the product in random was taken
         System.out.println("wishItemText = " + wishItemText);            // Written for checking
 
         wishButtons.get(randomSelection).click(); // The wish button of the relevant Random product was clicked
 
-        WebElement wishListLink= driver.findElement(By.xpath("//span[contains(text(),'Wish List')]"));
+        WebElement wishListLink = driver.findElement(By.xpath("//span[contains(text(),'Wish List')]"));
         wishListLink.click();
 
         //  Wish List navigated
-        List<WebElement> wishList=driver.findElements(By.xpath("//td[@class='text-left']/a"));
+        List<WebElement> wishList = driver.findElements(By.xpath("//td[@class='text-left']/a"));
 
         // Is the product we are looking for on the list or not?
-        boolean found=MyFunction.listContainsString(wishList, wishItemText);
+        boolean found = MyFunction.listContainsString(wishList, wishItemText);
 
-        Assert.assertTrue(found,"The product assigned to the Wish List could not be found.");
+        Assert.assertTrue(found, "The product assigned to the Wish List could not be found.");
     }
 }
