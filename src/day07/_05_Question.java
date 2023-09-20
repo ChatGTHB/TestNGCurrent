758eb3f41c7412349b73ef60778433fcc1e8a0f6
ChatGTHB <cihat.kose@hotmail.com>
2023-09-20 21:02:27 +0200

    Updated _05_Question

--- src/day07/_05_Question.java
+++ src/day07/_05_Question.java
@@ -12,7 +12,7 @@ import utility.MyFunction;
  1- Open the site.
  2- Search the word "ipod" on the site
  3- Add a random one of the results to the basket.
- 4- Click on Shopping Chart.
+ 4- Click on Shopping Cart.
  5- Verify that the selected product is in the basket.
  6- Solve with POM.
 
@@ -20,7 +20,7 @@ import utility.MyFunction;
  1- Siteyi açınız.
  2- Sitede "ipod" kelimesini aratınız
  3- Çıkan sonuçlardan random birini sepete atınız.
- 4- Shopping Chart a tıklatınız.
+ 4- Shopping Cart'a tıklatınız.
  5- Seçilen ürünün sepette olduğunu doğrulayınız.
  6- POM ile çözünüz.
  */
