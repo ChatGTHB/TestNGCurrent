f55e102d708eeeebf0c3ff80225a249b9f8c378b
ChatGTHB <cihat.kose@hotmail.com>
2023-09-20 20:27:08 +0200

    Updated _05_Question

--- src/day07/_05_Question.java
+++ src/day07/_05_Question.java
@@ -1,5 +1,12 @@
 package day07;
 
+import day06._04_PlaceOrder_Elements;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utility.BaseDriver;
+import utility.MyFunction;
+
 /**
  Scenario :
  1- Open the site.
@@ -18,5 +25,22 @@ package day07;
  6- POM ile çözünüz.
  */
 
-public class _05_Question {
+public class _05_Question extends BaseDriver {
+
+    @Test
+    public void addToCart() {
+
+        _04_PlaceOrder_Elements poe=new _04_PlaceOrder_Elements();
+        _03_WishListElements wle=new _03_WishListElements();
+        poe.searchBox.sendKeys("ipod" + Keys.ENTER);
+
+        int randomSecim= MyFunction.randomGenerator(wle.productList.size()); // A random number is generated
+        String cartItemText=wle.productList.get(randomSecim).getText(); // The name of the product in random was taken
+
+        wle.addToChart.get(randomSecim).click(); // The wish button of the relevant random product was clicked
+        wle.shoppingCart.click();
+
+        boolean bulundu=MyFunction.listContainsString(wle.wishList, cartItemText);
+        Assert.assertTrue(bulundu,"Sepet Listesinde aranan ürün bulunamadı");
+    }
 }
