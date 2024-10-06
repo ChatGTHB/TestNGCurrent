35493e7f4a898b9d53259c32f657f5ffd3feb0f3
ChatGTHB <cihat.kose@hotmail.com>
2024-10-07 00:10:37 +0200

    Edit _05_Intro.java

---
+++ src/day02/_05_Intro.java
@@ -0,0 +1,17 @@
+package day02;
+
+import org.testng.annotations.Test;
+
+public class _05_Intro {
+
+    @Test
+    public void _05_Intro_Test1() {
+        System.out.println("_05_Intro Test1");
+    }
+
+    @Test
+    public void _05_Intro_Test2() {
+        System.out.println("_05_Intro Test2");
+    }
+}
+
