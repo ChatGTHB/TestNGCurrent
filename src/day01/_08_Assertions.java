55782aa570f5f80164d8c6ca8401e1ab474c13f8
ChatGTHB <cihat.kose@hotmail.com>
2024-10-06 19:15:24 +0200

    Update

--- src/day01/_08_Assertions.java
+++ src/day01/_08_Assertions.java
@@ -5,53 +5,60 @@ import org.testng.annotations.Test;
 
 public class _08_Assertions {
 
-    @Test
+    @Test(priority = 1)
     public void equalsExample() {
-        String s1 = "Hello";
-        String s2 = "Hello1";
+        String string1 = "Hello";
+        String string2 = "Hello1";
 
         // Actual, Expected, Messsage (Optional)
-        // Assert.assertEquals(s1,s2);
-        Assert.assertEquals(s1, s2, "Actual and expected are not the same.");
+        // Assert.assertEquals(string1,string2);
+        Assert.assertEquals(string1, string2, "Actual and expected are not the same.");
     }
 
-    @Test
+    @Test(priority = 2)
     public void notEqualsExample() {
-        String s1 = "Hello";
-        String s2 = "Hello1";
+        String string1 = "Hello";
+        String string2 = "Hello1";
 
-        Assert.assertNotEquals(s1, s2, "Actual and expected are not the different.");
+        Assert.assertNotEquals(string1, string2, "Actual and expected are not the different.");
     }
 
-    @Test
+    @Test(priority = 3)
     public void trueExample() {
-        String s1 = "Hello";
-        String s2 = "Hello";
+        String string1 = "Hello";
+        String string2 = "Hello";
 
-        Assert.assertTrue(s1.equals(s2), "Actual and expected are not the same.");
+        Assert.assertEquals(string2, string1, "Actual and expected are not the same.");
     }
 
-    @Test
+    @Test(priority = 4)
     public void trueExample2() {
         int number1 = 5;
         int number2 = 5;
 
-        Assert.assertTrue(number1 == number2, "Actual and expected are not the same.");
+        Assert.assertEquals(number2, number1, "Actual and expected are not the same.");
     }
 
-    @Test
+    @Test(priority = 5)
     public void nullExample() {
         String s1 = null;
 
         Assert.assertNull(s1, "Actual, did not become NULL.");
     }
 
-    @Test
+    @Test(priority = 6)
     public void directFail() {
-        int a = 56;
+        int number = 56;
 
-        if (a != 55) {
+        if (number != 55) {
             Assert.fail();
         }
+    }
+
+    @Test(priority = 7)
+    public void falseExample() {
+        int number1 = 5;
+        int number2 = 7;
+        Assert.assertNotEquals(number2, number1, "Formed is not the same as expected");
     }
 }
